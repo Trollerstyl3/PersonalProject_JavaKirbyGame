f7e20b6a525e2a8907e99c4ed4a15baa00589d51
Khoi Nguyen <bob.k.nguyen@gmail.com>
2020-11-16 02:01:25 -0500

    Added texture to the enemy

--- KirbyGame/CharacterData.java
+++ KirbyGame/CharacterData.java
@@ -1,7 +1,5 @@
 import java.awt.image.*;
 import javax.imageio.*;
-import javax.swing.text.html.parser.Entity;
-import javax.xml.stream.events.EntityReference;
 
 import java.io.*;
 import java.awt.*;
@@ -34,6 +32,7 @@ public class CharacterData {
 
     BufferedImage[][] normalKirby;
     BufferedImage[][] knightKirby;
+    BufferedImage spriteSheetCharacter;
 
     boolean found;
     int imgCount,currentState;
@@ -57,6 +56,43 @@ public class CharacterData {
 
     public void loadImages(){
         normalKirby = new BufferedImage [24][];
+        initNormalKirby();
+
+        knightKirby = new BufferedImage [24][];
+        initKnightKirby();
+        
+        found = true;
+
+        spriteSheetCharacter = null;
+
+        File myFileCharacter = new File("KirbyGame/res/kirbys_character.png");
+
+        try {
+            spriteSheetCharacter = ImageIO.read(myFileCharacter);   
+        } 
+        catch (IOException e) {
+            found = false;
+            System.out.println("not found");
+        }
+
+        if(found){
+            
+            setNormalKirby();
+            
+            setKnightKirby();
+
+            if(entityType == EntityType.PLAYER){
+                currentImageArray = normalKirby[WALK_RIGHT];
+            }else if(entityType == EntityType.ENEMY){
+                currentImageArray = knightKirby[WALK_LEFT];
+            }
+            
+            oldImageArray = currentImageArray;
+
+        }
+    }
+
+    private void initNormalKirby (){
         normalKirby[WALK_RIGHT] = new BufferedImage[2];
         normalKirby[WALK_LEFT] = new BufferedImage[2];
         normalKirby[PAUSED_RIGHT] = new BufferedImage[3];
@@ -81,48 +117,9 @@ public class CharacterData {
         normalKirby[LOSE_LEFT] = new BufferedImage[1];
         normalKirby[STAND_RIGHT] = new BufferedImage[2];
         normalKirby[STAND_LEFT] = new BufferedImage[2];
+    }
 
-        knightKirby = new BufferedImage [24][];
-        knightKirby[WALK_RIGHT] = new BufferedImage[2];
-        knightKirby[PAUSED_RIGHT] = new BufferedImage[3];
-        knightKirby[SLAM_RIGHT] = new BufferedImage[1];
-        knightKirby[JUMP_RIGHT] = new BufferedImage[1];
-        knightKirby[FALL_RIGHT] = new BufferedImage[1];
-        knightKirby[CHARGE_RIGHT] = new BufferedImage[2];
-        knightKirby[ATTACK_RIGHT] = new BufferedImage[3];
-        knightKirby[HURT_RIGHT] = new BufferedImage[1];
-        knightKirby[PANIC_RIGHT] = new BufferedImage[2];
-        knightKirby[VICTORY_RIGHT] = new BufferedImage[7];
-        knightKirby[LOSE_RIGHT] = new BufferedImage[1];
-        knightKirby[STAND_RIGHT] = new BufferedImage[1]; 
-
-        knightKirby[WALK_LEFT] = new BufferedImage[2];
-        knightKirby[PAUSED_LEFT] = new BufferedImage[3];
-        knightKirby[SLAM_LEFT] = new BufferedImage[1];
-        knightKirby[JUMP_LEFT] = new BufferedImage[1];
-        knightKirby[FALL_LEFT] = new BufferedImage[1];
-        knightKirby[CHARGE_LEFT] = new BufferedImage[2];
-        knightKirby[ATTACK_LEFT] = new BufferedImage[3];
-        knightKirby[HURT_LEFT] = new BufferedImage[1];
-        knightKirby[PANIC_LEFT] = new BufferedImage[2];
-        knightKirby[VICTORY_LEFT] = new BufferedImage[7];
-        knightKirby[LOSE_LEFT] = new BufferedImage[1];
-        knightKirby[STAND_LEFT] = new BufferedImage[1];
-        found = true;
-
-        BufferedImage spriteSheetCharacter = null;
-
-        File myFileCharacter = new File("KirbyGame/res/kirbys_character.png");
-
-        try {
-            spriteSheetCharacter = ImageIO.read(myFileCharacter);   
-        } 
-        catch (IOException e) {
-            found = false;
-            System.out.println("not found");
-        }
-
-        if(found){
+    private void setNormalKirby(){
             normalKirby[STAND_RIGHT][0] = spriteSheetCharacter.getSubimage(4, 38, 22, 19);
             normalKirby[WALK_RIGHT][0] = spriteSheetCharacter.getSubimage(29, 37, 22, 20);
             normalKirby[WALK_RIGHT][1] = spriteSheetCharacter.getSubimage(54, 37, 22, 20);
@@ -202,21 +199,114 @@ public class CharacterData {
             normalKirby[VICTORY_LEFT][3] = flip(normalKirby[VICTORY_RIGHT][3]);
             normalKirby[VICTORY_LEFT][4] = flip(normalKirby[VICTORY_RIGHT][4]);
             normalKirby[VICTORY_LEFT][5] = flip(normalKirby[VICTORY_RIGHT][5]);
+    }
 
-            normalKirby[LOSE_LEFT][0] = flip(normalKirby[LOSE_RIGHT][0]);
+    private void initKnightKirby(){
+        knightKirby[WALK_RIGHT] = new BufferedImage[2];
+        knightKirby[PAUSED_RIGHT] = new BufferedImage[3];
+        knightKirby[SLAM_RIGHT] = new BufferedImage[1];
+        knightKirby[JUMP_RIGHT] = new BufferedImage[1];
+        knightKirby[FALL_RIGHT] = new BufferedImage[1];
+        knightKirby[CHARGE_RIGHT] = new BufferedImage[2];
+        knightKirby[ATTACK_RIGHT] = new BufferedImage[3];
+        knightKirby[HURT_RIGHT] = new BufferedImage[1];
+        knightKirby[PANIC_RIGHT] = new BufferedImage[2];
+        knightKirby[VICTORY_RIGHT] = new BufferedImage[7];
+        knightKirby[LOSE_RIGHT] = new BufferedImage[1];
+        knightKirby[STAND_RIGHT] = new BufferedImage[1]; 
+
+        knightKirby[WALK_LEFT] = new BufferedImage[2];
+        knightKirby[PAUSED_LEFT] = new BufferedImage[3];
+        knightKirby[SLAM_LEFT] = new BufferedImage[1];
+        knightKirby[JUMP_LEFT] = new BufferedImage[1];
+        knightKirby[FALL_LEFT] = new BufferedImage[1];
+        knightKirby[CHARGE_LEFT] = new BufferedImage[2];
+        knightKirby[ATTACK_LEFT] = new BufferedImage[3];
+        knightKirby[HURT_LEFT] = new BufferedImage[1];
+        knightKirby[PANIC_LEFT] = new BufferedImage[2];
+        knightKirby[VICTORY_LEFT] = new BufferedImage[7];
+        knightKirby[LOSE_LEFT] = new BufferedImage[1];
+        knightKirby[STAND_LEFT] = new BufferedImage[1];
+    }
+
+    private void setKnightKirby(){
+        normalKirby[LOSE_LEFT][0] = flip(normalKirby[LOSE_RIGHT][0]);
+
+        knightKirby[STAND_RIGHT][0] = spriteSheetCharacter.getSubimage(4, 275, 26, 30);
+
+        knightKirby[WALK_RIGHT][0] = spriteSheetCharacter.getSubimage(33, 274, 25, 31);
+        knightKirby[WALK_RIGHT][1] = spriteSheetCharacter.getSubimage(61, 273, 25, 32);
+
+        knightKirby[PAUSED_RIGHT][0] = spriteSheetCharacter.getSubimage(106, 271, 22, 34);
+        knightKirby[PAUSED_RIGHT][1] = spriteSheetCharacter.getSubimage(131, 270, 22, 35);
+        knightKirby[PAUSED_RIGHT][2] = spriteSheetCharacter.getSubimage(156, 269, 22, 36);
+
+        knightKirby[JUMP_RIGHT][0] = spriteSheetCharacter.getSubimage(266, 263, 23, 42);
+
+        knightKirby[FALL_RIGHT][0] = spriteSheetCharacter.getSubimage(266, 263, 23, 42);
+
+        knightKirby[SLAM_RIGHT][0] = spriteSheetCharacter.getSubimage(211, 276, 24, 29);
+
+        knightKirby[CHARGE_RIGHT][0] = spriteSheetCharacter.getSubimage(433, 274, 23, 31);
+        knightKirby[CHARGE_RIGHT][1] = spriteSheetCharacter.getSubimage(459, 272, 23, 33);
+    
+        knightKirby[ATTACK_RIGHT][0] = spriteSheetCharacter.getSubimage(598, 270, 33, 35);
+        knightKirby[ATTACK_RIGHT][1] = spriteSheetCharacter.getSubimage(634, 273, 46, 32);
+        knightKirby[ATTACK_RIGHT][2] = spriteSheetCharacter.getSubimage(683, 283, 44, 22);
+
+        knightKirby[HURT_RIGHT][0] = spriteSheetCharacter.getSubimage(818, 268, 22, 37);
+
+        knightKirby[PANIC_RIGHT][0] = spriteSheetCharacter.getSubimage(885, 275, 25, 30);
+        knightKirby[PANIC_RIGHT][1] = spriteSheetCharacter.getSubimage(913, 270, 24, 35);
+    
+        knightKirby[VICTORY_RIGHT][0] = spriteSheetCharacter.getSubimage(956, 263, 32, 42);
+        knightKirby[VICTORY_RIGHT][1] = spriteSheetCharacter.getSubimage(991, 275, 30, 30);
+        knightKirby[VICTORY_RIGHT][2] = spriteSheetCharacter.getSubimage(1023, 277, 28, 28);
+        knightKirby[VICTORY_RIGHT][3] = spriteSheetCharacter.getSubimage(1054, 273, 44, 32);
+        knightKirby[VICTORY_RIGHT][4] = spriteSheetCharacter.getSubimage(1101, 281, 44, 24);
+        knightKirby[VICTORY_RIGHT][5] = spriteSheetCharacter.getSubimage(1148, 280, 44, 25);
+        knightKirby[VICTORY_RIGHT][6] = spriteSheetCharacter.getSubimage(1195, 281, 44, 24);
+
+        knightKirby[LOSE_RIGHT][0] = spriteSheetCharacter.getSubimage(1246, 267, 23, 38);
 
 
+        knightKirby[STAND_LEFT][0] = flip(knightKirby[STAND_RIGHT][0]);
 
-            
-            if(entityType == EntityType.PLAYER){
-                currentImageArray = normalKirby[WALK_RIGHT];
-            }else if(entityType == EntityType.ENEMY){
-                currentImageArray = knightKirby[WALK_LEFT];
-            }
-            
-            oldImageArray = currentImageArray;
+        knightKirby[WALK_LEFT][0] = flip(knightKirby[WALK_RIGHT][0]);
+        knightKirby[WALK_LEFT][1] = flip(knightKirby[WALK_RIGHT][1]);
 
-        }
+        knightKirby[PAUSED_LEFT][0] = flip(knightKirby[PAUSED_RIGHT][0]);
+        knightKirby[PAUSED_LEFT][1] = flip(knightKirby[PAUSED_RIGHT][1]);
+        knightKirby[PAUSED_LEFT][2] = flip(knightKirby[PAUSED_RIGHT][2]);
+
+        //knightKirby[JUMP_LEFT][0] = spriteSheetFlipped.getSubimage(1076, 41, 26, 16);
+        knightKirby[JUMP_LEFT][0] = flip(knightKirby[JUMP_RIGHT][0]);
+
+        knightKirby[FALL_LEFT][0] = flip(knightKirby[FALL_RIGHT][0]);
+
+        knightKirby[SLAM_LEFT][0] = flip(knightKirby[SLAM_RIGHT][0]);
+
+        knightKirby[CHARGE_LEFT][0] = flip(knightKirby[CHARGE_RIGHT][0]);
+        knightKirby[CHARGE_LEFT][1] = flip(knightKirby[CHARGE_RIGHT][1]);
+
+        knightKirby[ATTACK_LEFT][0] = flip(knightKirby[ATTACK_RIGHT][0]);
+        knightKirby[ATTACK_LEFT][1] = flip(knightKirby[ATTACK_RIGHT][1]);
+        knightKirby[ATTACK_LEFT][2] = flip(knightKirby[ATTACK_RIGHT][2]);
+
+        knightKirby[HURT_LEFT][0] = flip(knightKirby[HURT_RIGHT][0]);
+
+        knightKirby[PANIC_LEFT][0] = flip(knightKirby[PANIC_RIGHT][0]);
+        knightKirby[PANIC_LEFT][1] = flip(knightKirby[PANIC_RIGHT][1]);
+
+        knightKirby[VICTORY_LEFT][0] = flip(knightKirby[VICTORY_RIGHT][0]);
+        knightKirby[VICTORY_LEFT][1] = flip(knightKirby[VICTORY_RIGHT][1]);
+        knightKirby[VICTORY_LEFT][2] = flip(knightKirby[VICTORY_RIGHT][2]);
+        knightKirby[VICTORY_LEFT][3] = flip(knightKirby[VICTORY_RIGHT][3]);
+        knightKirby[VICTORY_LEFT][4] = flip(knightKirby[VICTORY_RIGHT][4]);
+        knightKirby[VICTORY_LEFT][5] = flip(knightKirby[VICTORY_RIGHT][5]);
+        knightKirby[VICTORY_LEFT][5] = flip(knightKirby[VICTORY_RIGHT][6]);
+
+        knightKirby[LOSE_LEFT][0] = flip(knightKirby[LOSE_RIGHT][0]);
     }
 
     public void setCurrentState(int currentState) {
@@ -224,7 +314,11 @@ public class CharacterData {
 
         this.currentState = currentState;
         
-        currentImageArray = normalKirby[currentState];
+        if(entityType == EntityType.PLAYER){
+            currentImageArray = normalKirby[currentState];
+        }else if(entityType == EntityType.ENEMY){
+            currentImageArray = knightKirby[currentState];
+        }
     }
 
     public int getCurrentState(){
--- KirbyGame/CollisionChecker.java
+++ KirbyGame/CollisionChecker.java
@@ -12,7 +12,7 @@ public class CollisionChecker {
         return !areaA.isEmpty(); // Return true if the area is not empty
     }
 
-    public boolean checkWall(Shape shapeA, Shape shapeB){
+    public boolean checkHit(Shape shapeA, Shape shapeB){
         Area areaA = new Area(shapeA);
         areaA.intersect(new Area(shapeB)); // Compares if it is intersected
         return !areaA.isEmpty(); // Return true if the area is not empty
